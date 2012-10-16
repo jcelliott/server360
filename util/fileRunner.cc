b2e97cfe2a8724bfb707796e301f5c5c4065f99d
joshuacelliott <joshuacelliott@gmail.com>
2012-10-16 09:13:30 -0600

    added FileRunner

---
+++ util/fileRunner.cc
@@ -0,0 +1,18 @@
+/**
+ * Joshua Elliott
+ * fileRunner.cc
+ * CS360 - Lab 4 - Web Server
+ */
+
+#include "fileRunner.h"
+
+using namespace std;
+
+FileRunner::FileRunner() {
+
+}
+
+FileRunner::~FileRunner() {
+
+}
+
