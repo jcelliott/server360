76a2c4a6f1f738624ca1a5d7435cd0e07cfd715b
joshuacelliott <joshuacelliott@gmail.com>
2012-10-19 10:56:08 -0600

    Added readHead to handler [ci skip]

--- handler.cc
+++ handler.cc
@@ -6,44 +6,82 @@ Handler::Handler() {}
 
 Handler::~Handler() {}
 
-bool Handler::handle(int client) {
-    char *ptr;
+HTTPRequest* Handler::readHead() {
 
-    // read a request
-    memset(buf_,0,1501);
-    int nread = recv(client,buf_,1500,0);
+  string read = "";
+  leftover = "";
+  char *buf = (char *) calloc(BUFSIZE,sizeof(char));
+  int endOfHead = 0;
+
+  while ((endOfHead = read.find("\r\n\r\n")) == string::npos) {
+    memset(buf,0,BUFSIZE);
+    int nread = recv(sock,buf,BUFSIZE,0);
     if (nread < 0) {
-        if (errno == EINTR)
-            // the socket call was interrupted -- try again
-            return true;
-        // an error occurred so stop processing
-        perror("recv");
-        return false;
+      if (errno == EINTR)
+        // the socket call was interrupted -- try again
+        continue;
+      else
+        // an error occurred, so break out
+        if(debug) cout << "An error occurred in readHead()" << endl;
+      break;
     } else if (nread == 0) {
-        // the socket is closed
-        return false;
+      // the socket is closed
+      if(debug) cout << "The socket is closed in readHead()" << endl;
+      break;
     }
+    read.append(buf, nread);
 
-    // write the same data back
-    ptr = buf_;
-    int nwritten;
-    int nleft = nread;
-    while (nleft) {
-        if ((nwritten = send(client, ptr, nleft, 0)) < 0) {
-            if (errno == EINTR) {
-                // the socket call was interrupted -- try again
-                continue;
-            } else {
-                // an error occurred, so break out
-                perror("write");
-                return false;
-            }
-        } else if (nwritten == 0) {
-            // the socket is closed
-            return false;
-        }
-        nleft -= nwritten;
-        ptr += nwritten;
+    // if(debug) cout << "received: " << buf;
+  }
+  if(read.length() > endOfHead + 4) {
+    leftover = read.substr(endOfHead + 4);
+  }
+
+  // TODO: check this, it was just copypastad
+  HTTPRequest* req = new HTTPRequest();
+  req->parse(read);
+  if(debug) cout << "Response:\n" << req->str() << endl;
+  return req;
+}
+
+bool Handler::handle(int client) {
+  char *ptr;
+
+  // read a request
+  memset(buf_,0,1501);
+  int nread = recv(client,buf_,1500,0);
+  if (nread < 0) {
+    if (errno == EINTR)
+      // the socket call was interrupted -- try again
+      return true;
+    // an error occurred so stop processing
+    perror("recv");
+    return false;
+  } else if (nread == 0) {
+    // the socket is closed
+    return false;
+  }
+
+  // write the same data back
+  ptr = buf_;
+  int nwritten;
+  int nleft = nread;
+  while (nleft) {
+    if ((nwritten = send(client, ptr, nleft, 0)) < 0) {
+      if (errno == EINTR) {
+        // the socket call was interrupted -- try again
+        continue;
+      } else {
+        // an error occurred, so break out
+        perror("write");
+        return false;
+      }
+    } else if (nwritten == 0) {
+      // the socket is closed
+      return false;
     }
-    return true;
+    nleft -= nwritten;
+    ptr += nwritten;
+  }
+  return true;
 }
--- handler.h
+++ handler.h
@@ -1,24 +1,34 @@
 // C includes
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
 #include <iostream>
 
+#include "util/http/HTTPRequest.h"
+
+#define BUFSIZE 1024
+
 using namespace std;
 
 class Handler {
 public:
-    Handler();
-    ~Handler();
+  Handler();
+  ~Handler();
 
-    // Handle a client request, returns true if the server should keep handling
-    // requests for this client, otherwise returns false.
-    bool handle(int client);
+  // Handle a client request, returns true if the server should keep handling
+  // requests for this client, otherwise returns false.
+  bool handle(int client);
+
+  HTTPRequest* readHead();
 
 private:
-    char buf_[1600];
+  char buf_[1600];
+  string leftover;
+  int port, sock;
+  bool debug;
 };
 
